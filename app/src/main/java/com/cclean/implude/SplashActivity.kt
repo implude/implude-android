b2af531741d8bcee490ca4479163e5fb1ff9f9d3
wkdwhddn06 <wkdwhddn06@naver.com>
2020-02-25 17:18:10 +0900

    스플래쉬 액티비티 추가
    1. 스플래쉬 액티비티 추가함
    2. network_config 설정했는데 api23이라 어떻게 되는건지 모르겠다

---
+++ app/src/main/java/com/cclean/implude/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.cclean.implude
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
